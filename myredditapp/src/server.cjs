c193b63ff746f369be5aa9b898db33827dff9d79
Suzana Jelinic <suz_ana.j@outlook.com>
2024-06-03 12:24:17 -0400

    server.cjs code to connect app to Reddit API

---
+++ myredditapp/src/server.cjs
@@ -0,0 +1,10 @@
+/* oAuth2 code to connect my code to the reddit API */ 
+
+const axios = require('axios');
+
+const clientID = 'E3v4v0jXqg_1xjCmb1HvUw';
+const clientSecret = 'u1jOvsyDiTIsoWfQ-g_CeDrpbJ8YRQ';
+const redirectURI = 'http://localhost'
+const authorizationCode = 'EXPRESS JS AUTH CODE'
+
+
